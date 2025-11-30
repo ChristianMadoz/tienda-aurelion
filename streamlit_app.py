3e60d214f0c40918573b9aa3442453c03638e72b
Christian Madoz <116748661+ChristianMadoz@users.noreply.github.com>
2025-11-30 20:25:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ with st.expander('Map Visualization: Ventas por Provincia'):
 
     # 2. Cargar el archivo GeoJSON de Argentina (Provincias)
     # URL pública del GeoJSON desde datos.gob.ar
-    geojson_url = 'raw.githubusercontent.com'
+    geojson_url = 'https://infra.datos.gob.ar/georef/provincias'
     
     # Intentamos cargar el GeoJSON
     try:
