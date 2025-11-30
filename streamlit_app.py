edbf3154e42dd81f32fafbefd0e4cf6a8c871a12
Christian Madoz <116748661+ChristianMadoz@users.noreply.github.com>
2025-11-30 15:27:48 -0300

    Update data loading with expander for improved layout
    Refactor data loading to use expander for better UI.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,9 @@ st.title('🎈 Machine Learning Tienda Aurelion App')
 
 st.info('Modelo de Machine Learning para tienda!')
 
-# Use the "raw" GitHub URL instead of the "blob" URL
-#csv_url = 'https://raw.githubusercontent.com/ChristianMadoz/data/refs/heads/main/df_aurelion.csv'
-#df0 = pd.read_csv(csv_url)
+with st.expander('Data'):
+  st.write('**RawData**')
+  # Use the "raw" GitHub URL instead of the "blob" URL
+  df = pd.read_csv('https://raw.githubusercontent.com/ChristianMadoz/data/refs/heads/main/dataset_ventas_unificado_completo.csv')
+  df
 
-df = pd.read_csv('https://raw.githubusercontent.com/ChristianMadoz/data/refs/heads/main/dataset_ventas_unificado_completo.csv')
-df
-#df0
