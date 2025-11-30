5d4e2ea6a81095ee51409f3794964122328deb08
Christian Madoz <116748661+ChristianMadoz@users.noreply.github.com>
2025-11-30 16:04:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,12 @@ with st.expander('Data'):
   # Use the "raw" GitHub URL instead of the "blob" URL
   df = pd.read_csv('https://raw.githubusercontent.com/ChristianMadoz/data/refs/heads/main/dataset_ventas_unificado_completo.csv')
   df
-
   st.write('**X**')
   x = df.drop('nombre_producto', axis = 1)
   x
-
-
   st.write('**Y**')
   y = df.nombre_producto
   y
-  
+
+with st.expander('Data Visualization'):
+  st.scatter_chart(data=df, x = 'productos', y = 'fecha', color = 'ciudad')
