b6043972f394f3d8040ac348313195dd9851c432
Christian Madoz <116748661+ChristianMadoz@users.noreply.github.com>
2025-11-30 13:43:11 -0300

    Import pandas and load sales dataset from CSV
    Added pandas import and CSV loading for sales dataset.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title('🎈 Machine Learning Tienda Aurelion App')
 
 st.info('Modelo de Machine Learning para tienda!')
+
+pd.read_csv('https://github.com/ChristianMadoz/data/blob/main/dataset_ventas_unificado_completo.csv')
+df
