33369542222367eb53292fdd3c32d567a19d5da4
Christian Madoz <116748661+ChristianMadoz@users.noreply.github.com>
2025-11-30 15:08:28 -0300

    Fix CSV URL for reading data in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ st.title('🎈 Machine Learning Tienda Aurelion App')
 st.info('Modelo de Machine Learning para tienda!')
 
 # Use the "raw" GitHub URL instead of the "blob" URL
-csv_url = 'https://github.com/ChristianMadoz/data/blob/main/df_aurelion.csv?plain=1'
-df = pd.read_csv(csv_url)
+#csv_url = 'https://github.com/ChristianMadoz/data/blob/main/df_aurelion.csv?plain=1'
+#df = pd.read_csv(csv_url)
 
-#df = pd.read_csv('https://github.com/ChristianMadoz/data/blob/main/df_aurelion.csv')
+df = pd.read_csv('https://github.com/ChristianMadoz/data/blob/main/df_aurelion.csv?plain=1')
 df
