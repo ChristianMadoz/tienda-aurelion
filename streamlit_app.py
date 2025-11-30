ed24fdd507689483c71035c3f586d44f0635579d
Christian Madoz <116748661+ChristianMadoz@users.noreply.github.com>
2025-11-30 15:22:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ st.title('🎈 Machine Learning Tienda Aurelion App')
 st.info('Modelo de Machine Learning para tienda!')
 
 # Use the "raw" GitHub URL instead of the "blob" URL
-csv_url = 'https://raw.githubusercontent.com/ChristianMadoz/data/refs/heads/main/df_aurelion.csv'
-df0 = pd.read_csv(csv_url)
+#csv_url = 'https://raw.githubusercontent.com/ChristianMadoz/data/refs/heads/main/df_aurelion.csv'
+#df0 = pd.read_csv(csv_url)
 
 df = pd.read_csv('https://raw.githubusercontent.com/ChristianMadoz/data/refs/heads/main/dataset_ventas_unificado_completo.csv')
 df
-df0
+#df0
