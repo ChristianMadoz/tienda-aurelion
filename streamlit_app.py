20480fe23e6a64cd49ca0b794a3831ea0431e47d
Christian Madoz <116748661+ChristianMadoz@users.noreply.github.com>
2025-11-30 13:44:25 -0300

    Fix data loading by assigning to df

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ st.title('🎈 Machine Learning Tienda Aurelion App')
 
 st.info('Modelo de Machine Learning para tienda!')
 
-pd.read_csv('https://github.com/ChristianMadoz/data/blob/main/dataset_ventas_unificado_completo.csv')
+df = pd.read_csv('https://github.com/ChristianMadoz/data/blob/main/dataset_ventas_unificado_completo.csv')
 df
