ad37bb569daafe16a9a98c754410f8c130ff1ac9
Christian Madoz <116748661+ChristianMadoz@users.noreply.github.com>
2025-12-01 00:58:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ with st.expander('Data'):
     st.dataframe(y)
     
     st.write('Ventas por ciudad')
-    ventas por ciudad = df.ventas_por_depto
-    st.dataframe(ventas_por_depto)
+    ventas = df.ventas_por_depto
+    st.dataframe(ventas)
 
 with st.expander('Data Visualization'):
     st.bar_chart(data=df, x='ciudad', y='importe', use_container_width=True)
